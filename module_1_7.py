535cd4901379e26913bbf857b4002d6c43936e2f
xHazeex <nda41550@gmail.com>
2024-11-12 22:30:17 +0300

    Add files via upload

---
+++ module_1_7.py
@@ -0,0 +1,8 @@
+immutable_var = 1, "Питон тема", 12
+print (immutable_var)
+# Кортеж нельзя изменить, это его отличает от списка
+
+mutable_list = [1, 2, 3, 4]
+print (mutable_list)
+mutable_list.remove(4)
+print (mutable_list)
